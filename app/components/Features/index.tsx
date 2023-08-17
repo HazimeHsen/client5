e304b89be3599d0c1f403f8ec7bbf75ed0ece918
Hsen-123 <hazimehussein01@gmail.com>
2023-08-17 17:54:27 +0300

    first commit

--- app/components/Features/index.tsx
+++ app/components/Features/index.tsx
@@ -44,19 +44,20 @@ const Features = () => {
         <div className="flex justify-center">
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-4">
             {featuresdata.map((items, i) => (
-              <div className="max-w-sm border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <div
+                key={i}
+                className="max-w-sm border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                 <a href="#">
                   <img className="rounded-t-lg" src="/images/blog.jpg" alt="" />
                 </a>
                 <div className="p-5">
                   <a href="#">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                      Noteworthy technology acquisitions 2021
+                      {items.heading}
                     </h5>
                   </a>
                   <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                    Here are the biggest enterprise technology acquisitions of
-                    2021 so far, in reverse chronological order.
+                    {items.subheading}
                   </p>
                 </div>
               </div>
